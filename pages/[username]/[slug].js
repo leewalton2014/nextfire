9d3c57802ff7c440dd09955faf72f81dc816fbb1
Lee Walton <lee.walton2014@outlook.com>
2021-08-29 04:33:06 +0100

    post content

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -1,3 +1,8 @@
+import styles from "../../styles/Post.module.css";
+import PostContent from "../../components/PostContent";
+import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+
 export default function PostPage({}) {
     return (
         <main>
