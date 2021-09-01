52f652fa8e4aeef20eb370f94f5667960d1e89e9
Lee Walton <lee.walton2014@outlook.com>
2021-09-01 03:27:24 +0100

    likes and claps

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -2,6 +2,9 @@ import styles from "../../styles/Post.module.css";
 import PostContent from "../../components/PostContent";
 import { firestore, getUserWithUsername, postToJSON } from "../../lib/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
+import HeartButton from "../../components/HeartButton";
+import AuthCheck from "../../components/AuthCheck";
+import Link from "next/link";
 
 export async function getStaticProps({ params }) {
     const { username, slug } = params;
@@ -14,7 +17,7 @@ export async function getStaticProps({ params }) {
         const postRef = userDoc.ref.collection("posts").doc(slug);
         post = postToJSON(await postRef.get());
         path = postRef.path;
-    }else{
+    } else {
         return {
             notFound: true,
         };
@@ -63,6 +66,15 @@ export default function Post(props) {
                 <p>
                     <strong>{post.heartCount || 0} 🤍</strong>
                 </p>
+                <AuthCheck
+                    fallback={
+                        <Link href="/enter">
+                            <button>💗 Sign Up</button>
+                        </Link>
+                    }
+                >
+                    <HeartButton postRef={postRef} />
+                </AuthCheck>
             </aside>
         </main>
     );
